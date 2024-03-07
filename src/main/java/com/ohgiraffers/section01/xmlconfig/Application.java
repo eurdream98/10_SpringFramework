a212ae9f6dac265793304def993317f3958c0619
eurdream98 <eurdream98@naver.com>
2024-03-07 17:04:12 +0900

    2024_03_07 Spring

---
+++ src/main/java/com/ohgiraffers/section01/xmlconfig/Application.java
@@ -0,0 +1,18 @@
+package com.ohgiraffers.section01.xmlconfig;
+
+import com.ohgiraffers.common.MemberDTO;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class Application {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext
+                = new GenericXmlApplicationContext("section01/xmlconfig/spring-context.xml");
+        MemberDTO member = applicationContext.getBean("member",MemberDTO.class);
+        System.out.println(member.getPersonalAccount());
+        System.out.println(member.getPersonalAccount().deposit(10000));
+        System.out.println(member.getPersonalAccount().getBalance());
+        System.out.println(member.getPersonalAccount().withDraw(5000));
+        System.out.println(member.getPersonalAccount().getBalance());
+    }
+}
